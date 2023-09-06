63960b9dc480191a089f947425068cc2babbfa6d
강현욱 <bug6868@naver.com>
2023-09-06 16:55:10 +0900

    페이징 및 정렬

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -7,6 +7,7 @@ import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.security.UserDetailsImpl;
 import com.sparta.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,12 +31,14 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    public List<ProductResponseDto> getProducts(@AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return productService.getProduct(userDetails.getUser());
+    public Page<ProductResponseDto> getProducts( // 클라이언트에서 보내는 페이징, 정렬 관련 데이터 받아옴
+         @RequestParam("page") int page, // 클라이언트에서 1부터 받아옴, 서버에선 0으로 처리해야
+         @RequestParam("size") int size,
+         @RequestParam("sortBy") String sortBy,
+         @RequestParam("isAsc") boolean isAsc,
+         @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        return productService.getProduct(userDetails.getUser(), page-1, size, sortBy, isAsc);
     }
 
-    @GetMapping("/admin/products")
-    public List<ProductResponseDto> getAllProducts() {
-        return productService.getAllProducts();
-    }
 }
